80171df2fa07a7986ae35d506ad1ffb1eb3ccfd5
Michael Osgood <michaelosgood@dhcp-v120-099.mobile.uci.edu>
2017-10-22 20:52:15 -0700

    updated the look

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,17 +1,36 @@
 // Michael Osgood / Web Scraping Application
 
 // Grab the articles as a json
+
+  //MY TEST==------------------||---==--==--
+  //MY TEST==------------------||---==--==--
+  //MY TEST==---------------\\-||-//==--==--
+  //MY TEST==----------------\\||//-==--==--
+  //MY TEST==-----------------\\//--==--==--
+  //MY TEST==------------------\/---==--==--
+  // Grab the articles as a json
 $.getJSON("/articles", function(data) {
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].headline + "<br />" + data[i].summary + "<br />" + data[i].url + "</p>");
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+    // Display the apropriate information on the page
+    $("#articles").append(
+      "<div class='panel panel-default'><div class='panel-heading'><p data-id='" + data[i]._id + "'>"
+      +data[i].headline+"</p></div><div class='panel-body'><p>"
+      +data[i].summary +"</p><br>"
+      +"<a class='btn btn-info save'>Save Article</a><br><br>"
+      +"<a target='_blank' href='" + data[i].url + "' class='btn btn-info view'>Go to Article</a><br><br>"
+      +"<a data-id='" + data[i]._id + "' class='btn btn-info'>Add Note</a><br><br><hr></div><div id='notes'></div></div>");
     }
-  });
-  
+});
+  //MY TEST==------------------/\---==--==--
+  //MY TEST==-----------------//\\--==--==--
+  //MY TEST==----------------//||\\-==--==--
+  //MY TEST==---------------//-||-\\==--==--
+  //MY TEST==------------------||---==--==--
+  //MY TEST==------------------||---==--==--
   
   // Whenever someone clicks a p tag
-  $(document).on("click", "p", function() {
+  $(document).on("click", ".btn", function() {
     // Empty the notes from the note section
     $("#notes").empty();
     // Save the id from the p tag
@@ -25,14 +44,16 @@ $.getJSON("/articles", function(data) {
       // With that done, add the note information to the page
       .done(function(data) {
         console.log(data);
+        
+        $("#notes").append("<div class='panel'>Panel</div>");
         // The title of the article
-        $("#notes").append("<h3>" + data.headline + "</h3>");
+        $("#notes").append("<h7>" + data.headline + "</h7>");
         // An input to enter a new title
         $("#notes").append("<input id='titleinput' name='title' >");
         // A textarea to add a new note body
         $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
         // A button to submit a new note, with the id of the article saved to it
-        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+        $("#notes").append("<button class='btn btn-info'data-id='" + data._id + "' id='savenote'>Save Note</button>");
   
         // If there's a note in the article
         if (data.note) {
