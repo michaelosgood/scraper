86089828255521b7857d7f31656d81155289cea7
Michael Osgood <michaelosgood@dhcp-v120-099.mobile.uci.edu>
2017-11-04 17:38:01 -0700

    updating repo for heroku deployment

--- server.js
+++ server.js
@@ -150,7 +150,7 @@ app.post("/articles/:id", function(req, res) {
 });
 
 
-// Listen on port 3000
+// Listen on port 4000
 app.listen(4000, function() {
-  console.log("App running on port 3000!");
+  console.log("App running on port 4000!");
 });
